34b46bfc21bb4716dd9cfe7919d34611a503052c
段廷银 <clickyeah@yeah.net>
2015-03-11 08:59:19 +0800

    上传脚本

---
+++ up.sh
@@ -0,0 +1,10 @@
+ssh -T git@github.com
+#git clone https://github.com/dtya/CV
+git init
+touch up.sh
+git add up.sh
+git commit -m '上传脚本'
+git remote rm origin
+git remote add origin https://github.com/dtya/CV
+git push origin master 
+
