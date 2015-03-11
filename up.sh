38bbe3be90c703113c768b15d2e14e2964365cc6
段廷银 <clickyeah@yeah.net>
2015-03-11 09:27:07 +0800

    简历2015.03.06

--- up.sh
+++ up.sh
@@ -1,9 +1,9 @@
 ssh -T git@github.com
 #git clone https://github.com/dtya/CV
 git init
-touch up.sh
+touch '段廷银.pdf'
 git add up.sh
-git commit -m '上传脚本'
+git commit -m '简历2015.03.06'
 git remote rm origin
 git remote add origin https://github.com/dtya/CV
 git push origin master 
